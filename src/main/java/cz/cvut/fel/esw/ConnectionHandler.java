fb83733bf4fe985d76cece0e709cbd2e0d611a78
JakubJíra <jirajak2@fel.cvut.cz>
2022-05-26 00:32:22 +0200

    handleGetCount works, but with RuntimeException.

--- src/main/java/cz/cvut/fel/esw/ConnectionHandler.java
+++ src/main/java/cz/cvut/fel/esw/ConnectionHandler.java
@@ -1,10 +1,19 @@
 package cz.cvut.fel.esw;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.Set;
+import java.util.zip.GZIPInputStream;
+
+import cz.cvut.fel.esw.proto.*;
+
+import javax.xml.stream.FactoryConfigurationError;
 
 public class ConnectionHandler implements Runnable {
     private final Socket clientSocket;
@@ -16,24 +25,82 @@ public class ConnectionHandler implements Runnable {
 
     @Override
     public void run() {
+        InputStream inputStream;
+        OutputStream outputStream;
         try {
-            InputStream inputStream = this.clientSocket.getInputStream();
+            inputStream = this.clientSocket.getInputStream();
         } catch (IOException e) {
             throw new RuntimeException("Error connecting to sockets input stream", e);
         }
         try {
-            OutputStream outputStream = this.clientSocket.getOutputStream();
+            outputStream = this.clientSocket.getOutputStream();
         } catch (IOException e) {
             throw new RuntimeException("Error connecting to sockets output stream", e);
         }
-        boolean connectionOK;
-        do {
-            connectionOK = this.handleConnection();
-        } while (connectionOK);
+        try {
+            boolean connectionOK;
+            do {
+                connectionOK = this.handleConnection(inputStream, outputStream);
+            } while (connectionOK);
+        } catch (IOException e) {
+            throw new RuntimeException("Error while handling connection");
+        }
+
     }
 
-    private boolean handleConnection() {
-        System.out.println("Got connection!");
-        return false;
+    private boolean handleConnection(InputStream inputStream, OutputStream outputStream) throws IOException {
+        int msgSize;
+        try {
+            msgSize = this.getMessageSize(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException("Error getting message size...");
+        }
+        if (msgSize == -1){
+            return false;
+        }
+        byte [] messageBody = getMessage(inputStream, msgSize);
+        Request request = Request.parseFrom(messageBody);
+        if (request.hasGetCount()) {
+            handleGetCount(outputStream);
+        } else {
+            handlePostWords(request, outputStream);
+        }
+        return true;
     }
+
+    private int getMessageSize(InputStream inputStream) throws IOException {
+        byte sizeBytes [] = new byte [Utils.MESSAGE_LENGTH];
+        inputStream.read(sizeBytes, 0, Utils.MESSAGE_LENGTH);
+        return Utils.arrToInt(sizeBytes);
+    }
+
+    private byte [] getMessage(InputStream inputStream, int msgSize) throws IOException {
+        byte [] messageBody = new byte[msgSize];
+        int accepted_count = 0;
+        while(accepted_count != msgSize){
+            if (inputStream.available() > msgSize-accepted_count) {
+                accepted_count += inputStream.read(messageBody, accepted_count, msgSize-accepted_count);
+            } else {
+                accepted_count += inputStream.read(messageBody, accepted_count, inputStream.available());
+            }
+        }
+        return messageBody;
+    }
+    private void handleGetCount(OutputStream os) throws IOException {
+        Response response = Response.newBuilder()
+                .setCounter(this.stringDatabase.size())
+                .setStatus(Response.Status.OK)
+                .build();
+        int sizeInt = response.getSerializedSize();
+        byte[] sizeBytes = ByteBuffer.allocate(Utils.MESSAGE_LENGTH).putInt(sizeInt).array();
+        os.write(sizeBytes);
+        response.writeTo(os);
+        this.stringDatabase.clear();
+    }
+
+    private void handlePostWords(Request request, OutputStream os) throws IOException {
+        throw new EOFException ("To be implemented");
+    }
+
+
 }
--- src/main/java/cz/cvut/fel/esw/Utils.java
+++ src/main/java/cz/cvut/fel/esw/Utils.java
@@ -1,6 +1,7 @@
 package cz.cvut.fel.esw;
 
 public class Utils {
+    public static final int MESSAGE_LENGTH = 4;
     public static boolean isPort(String arg) {
         int i;
         try {
@@ -12,5 +13,11 @@ public class Utils {
             return false;
         }
         return true;
+    }
+    public static int arrToInt(byte[] bytes) {
+        return ((bytes[0] & 0xFF) << 24) |
+                ((bytes[1] & 0xFF) << 16) |
+                ((bytes[2] & 0xFF) << 8) |
+                ((bytes[3] & 0xFF));
     }
 }
